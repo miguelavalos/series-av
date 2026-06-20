74fefd62f65e6399ec7e1d49fa2eaa794e8cdee5
Miguel Angel Avalos Fernandez <miguelavalos@gmail.com>
2026-06-20 14:25:16 +0200

    Add Series AV universal link AASA

--- apps/web/src/start.ts
+++ apps/web/src/start.ts
@@ -1,6 +1,36 @@
 import { accountAvMiddleware } from "@avalsys/account-av-web";
-import { createStart } from "@tanstack/react-start";
+import { createMiddleware, createStart } from "@tanstack/react-start";
+
+const appleAppSiteAssociation = {
+  applinks: {
+    details: [
+      {
+        appIDs: ["935PM55U6R.com.avalsys.seriesav.dev", "935PM55U6R.com.avalsys.seriesav"],
+        components: [
+          {
+            "/": "/i/r/*",
+            comment: "Series AV private share invite links"
+          }
+        ]
+      }
+    ]
+  }
+};
+
+const appleAppSiteAssociationMiddleware = createMiddleware().server(async ({ next, request }) => {
+  const url = new URL(request.url);
+  if (url.pathname !== "/.well-known/apple-app-site-association") {
+    return next();
+  }
+
+  return Response.json(appleAppSiteAssociation, {
+    headers: {
+      "cache-control": "public, max-age=0, must-revalidate",
+      "content-type": "application/json"
+    }
+  });
+});
 
 export const startInstance = createStart(() => ({
-  requestMiddleware: [accountAvMiddleware()]
+  requestMiddleware: [appleAppSiteAssociationMiddleware, accountAvMiddleware()]
 }));
